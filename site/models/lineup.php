b4d26892ca4f54d86bbe66b72168e58d239eb3a3
Zensoji <zensoji@gmail.com>
2013-08-19 10:22:17 +0100

    2 Model Changes left to do in /models/: 	- leaderboards.php 	- teamadmin.php 	- teammembers.php 	- teams.php

--- site/models/lineup.php
+++ site/models/lineup.php
@@ -1,7 +1,155 @@
 <?php
+/***************************************************************************
+ * 
+ * PvP Boot Camp Leaderboards Component
+ * @comname         com_bootcampleaderboards
+ * @package         Bootcampleaderboards.Site
+ * @subpackage      Views
+ *
+ * @copyright       (C) Copyright 2013 PvP Boot Camp. All rights reserved.
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            http://www.pvp-boot-camp.com
+ * @author          Alex Armstrong
+ *                  Lynton Steyn
+ * @author-email    zensoji@pvp-boot-camp.com
+ *                  drkfrontiers@pvp-boot-camp.com
+ * 
+ ***************************************************************************
+ * 
+ * Site's Edit a Match's Lineup Model.
+ * lineup.php 
+ * 
+ **************************************************************************/
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla modelitem library
+jimport('joomla.application.component.modelform');
+
+require_once JPATH_SITE.DIRECTORY_SEPARATOR.'administrator'.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_bootcampleaderboards'.DIRECTORY_SEPARATOR.'helpers'.DIRECTORY_SEPARATOR.'bootcampleaderboardshelper.php';
+
+class BootcampleaderboardsModelLineup extends JModelForm
+{
+	public function getTable($type = 'Lineup', $prefix = 'BootcampleaderboardsTable', $config = array()) 
+	{
+		return JTable::getInstance($type, $prefix, $config);
+	}
+	
+	public function getForm($data = array(), $loadData = true) 
+	{
+		// Get the form.
+		$form = $this->loadForm('com_bootcampleaderboards.lineup', 'lineup', array('control' => 'jform', 'load_data' => $loadData));
+		if (empty($form)) 
+		{
+			return false;
+		}
+		return $form;
+	}
+
+	protected function loadFormData() 
+	{
+		// Check the session for previously entered form data.
+		$data = JFactory::getApplication()->getUserState('com_bootcampleaderboards.lineup.data', array());
+		if (empty($data)) 
+		{
+			$lineup_id = JRequest::getVar( 'lineup_id', '', 'default', 'int' );
+
+			$data = $this->getData($lineup_id);
+                        
+		}
+		return $data;
+	}
+	
+	public function getData($lineup_id)
+	{		
+		$query = ' SELECT * from #__bootcamp_leaderboards_matches_lineups WHERE lineup_id = '.$lineup_id;
+		$this->_db->setQuery( $query );
+		$data = $this->_db->loadObject();
+                $team_id  = $data['lineup_team_id'];
+		
+		if ($data == null)
+		{
+			return null;	
+		}
+		// load members
+		$data->members = $this->getMemberList($team_id);
+		
+		return $data;
+	}
+	
+	function getMemberList($team_id)
+	{				
+            $query =  "SELECT me.username, me.cb_handle, me.avatar, me.cb_team_memberstate, me.cb_team_join_date, me.cb_team_staff_rank";
+            $query .= " FROM #__comprofiler me";
+            $query .= " WHERE me.cb_team_id='.$team_id.'";
+            $query .= " ORDER BY me.cb_team_staff_rank DESC";
+            
+            $db = $this->getDbo();
+		
+            $db->setQuery($query); 
+            return $db->loadObjectList();
+	}
+        
+        function getCharlist($user_id)
+        {
+            $query  = "SELECT *";
+            $query .= " FROM #__bootcamp_leaderboards_chars";
+            $query .= " WHERE char_user_id='.$user_id.'";
+            $query .= " ORDER BY char_name";
+            
+            $db = $this->getDbo();
+            
+            $db->setQuery($query);
+            return $db->loadObjectList();
+        }
+	
+	public function updateItem($data)
+	{
+		$db = JFactory::getDbo();		
+		$query = $db->getQuery(true);
+		
+		$query->update('#__bootcamp_leaderboards_matches_lineups li');
+                $query->set('li.lineup_team_member_1 = '.$db->quote($data['tm1_id'])); 
+                $query->set('li.lineup_team_member_1_name = '.$db->quote($data['tm1_name'])); 
+                $query->set('li.lineup_team_member_1_char = '.$db->quote($data['tm1_char']));
+                $query->set('li.lineup_team_member_1_role = '.$db->quote($data['tm1_role']));
+                
+                $query->set('li.lineup_team_member_2 = '.$db->quote($data['tm2_id'])); 
+                $query->set('li.lineup_team_member_2_name = '.$db->quote($data['tm2_name'])); 
+                $query->set('li.lineup_team_member_2_char = '.$db->quote($data['tm2_char'])); 
+                $query->set('li.lineup_team_member_2_role = '.$db->quote($data['tm2_role']));
+                
+                $query->set('li.lineup_team_member_3 = '.$db->quote($data['tm3_id']));
+                $query->set('li.lineup_team_member_3_name = '.$db->quote($data['tm3_name']));
+                $query->set('li.lineup_team_member_3_char = '.$db->quote($data['tm3_char']));
+                $query->set('li.lineup_team_member_3_role = '.$db->quote($data['tm3_role']));
+                
+                $query->set('li.lineup_team_member_4 = '.$db->quote($data['tm4_id']));
+                $query->set('li.lineup_team_member_4_name = '.$db->quote($data['tm4_name']));
+                $query->set('li.lineup_team_member_4_char = '.$db->quote($data['tm4_char']));
+                $query->set('li.lineup_team_member_4_role = '.$db->quote($data['tm4_role']));
+                
+                $query->set('li.lineup_team_member_5 = '.$db->quote($data['tm5_id']));
+                $query->set('li.lineup_team_member_5_name = '.$db->quote($data['tm5_name']));
+                $query->set('li.lineup_team_member_5_char = '.$db->quote($data['tm5_char']));
+                $query->set('li.lineup_team_member_5_role = '.$db->quote($data['tm5_role']));
+			
+		$query->where('li.lineup_id = ' . (int)$data['lineup_id']);
+		$db->setQuery($query);
+		
+		$db->query();	
+		
+		if ($db->getErrorMsg()) 
+		{
+			JError::raiseError(500, $db->getErrorMsg());
+			return false;
+		} 
+		else 
+		{
+			return true;
+		}
+	}
+}
+
 ?>
--- site/models/result.php
+++ site/models/result.php
@@ -1,7 +1,202 @@
 <?php
+/***************************************************************************
+ * 
+ * PvP Boot Camp Leaderboards Component
+ * @comname         com_bootcampleaderboards
+ * @package         Bootcampleaderboards.Site
+ * @subpackage      Views
+ *
+ * @copyright       (C) Copyright 2013 PvP Boot Camp. All rights reserved.
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            http://www.pvp-boot-camp.com
+ * @author          Alex Armstrong
+ *                  Lynton Steyn
+ * @author-email    zensoji@pvp-boot-camp.com
+ *                  drkfrontiers@pvp-boot-camp.com
+ * 
+ ***************************************************************************
+ * 
+ * Site's Report A Result Model.
+ * result.php 
+ * 
+ **************************************************************************/
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla modelform library
+jimport('joomla.application.component.modelform');
+
+require_once JPATH_SITE.DIRECTORY_SEPARATOR.'administrator'.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_bootcampleaderboards'.DIRECTORY_SEPARATOR.'helpers'.DIRECTORY_SEPARATOR.'bootcampleaderboardshelper.php';	
+
+class BootcampleaderboardsModelResult extends JModelForm
+{
+	protected function populateState()
+	{		
+		$params = JFactory::getApplication()->getParams();
+		$this->setState('params', $params);
+	}
+	
+	public function getForm($data = array(), $loadData = true) 
+	{
+		// Get the form.
+		$form = $this->loadForm('com_bootcampleaderboards.result', 'result', array('control' => 'jform', 'load_data' => $loadData));
+		if (empty($form)) 
+		{
+			return false;
+		}
+		
+		return $form;
+	}
+	
+	protected function loadFormData()
+	{
+		$data = (array)JFactory::getApplication()->getUserState('com_bootcampleaderboards.result.data', array());
+		return $data;
+	}
+        
+        public function getLeaderboardList($namepartleaderboard,$data)
+        {
+            $db = JFactory::getDbo();
+            $query = "SELECT * FROM #__bootcamp_leaderboards WHERE leaderboard_published = 1 AND leaderboard_name like '%".$namepartleaderboard."%' ORDER BY leaderboard_name";
+            
+            $db->setQuery($query,0,10);
+            return $db->loadObjectList();
+            
+            $leaderboard  = $data['leaderboard_id'];
+        }
+        
+        public function getMatchList($namepartmatch,$leaderboard)
+        {
+            $db = JFactory::getDbo();
+            
+            $query = "SELECT * FROM #__bootcamp_leaderboards_matches WHERE match_published = 1 AND match_leaderboard_id = '.$db->Quote($leaderboard).' AND match_name like '%".$namepartmatch."%' ORDER BY match_name";
+            
+            $db->setQuery($query,0,10);
+            return $db->loadObjectList();
+            
+            $team1 = $data['match_team1_id'];
+            $team2 = $data['match_team2_id'];
+        }
+	
+	public function getTeamList($namepartteam,$team1,$team2)
+	{
+		$db = JFactory::getDBO();
+		$query = "SELECT * FROM #__bootcamp_leaderboards_teams WHERE team_published = 1 AND team_name like '%".$namepartteam."%' AND team_name = '.$team1.' OR team_name = '.$team2.' order by team_name";
+		
+		$db->setQuery($query,0,10); 
+		return $db->loadObjectList();
+	}
+        
+        public function getMemberList($namepartmember,$data)
+        {
+            $db = JFactory::getDbo();
+            $team = $data['team_id'];
+            
+            $query = "SELECT * FROM #__comprofiler WHERE cb_team_id='.$team.' AND cb_team_memberstate != 0 ORDER BY username";
+            
+            $db->setQuery($query,0,10);
+            return $db->loadObjectList();
+        }
+	
+	public function insertItem($data)
+	{			
+		// set the data into a query to update the record
+		$db	= JFactory::getDBO();
+                
+                $user = JFactory::getUser();
+                $userid = $user->get('id');
+		
+		// try to find the leaderboard
+		$query = $db->getQuery(true);
+		$query->select('*');
+		$query->from('#__bootcamp_leaderboards');
+		$query->where('leaderboard_name='.$db->Quote($data['leaderboard_name']));
+		$db->setQuery($query);
+		$leaderboardInfo = $db->loadObject();
+                
+                // try to find the match
+                $query->select('*');
+		$query->from('#__bootcamp_leaderboards_matches');
+		$query->where('match_id='.$db->Quote($data['match_id']));
+		$db->setQuery($query);
+		$matchInfo = $db->loadObject();
+                
+                // try to find the team
+                $query->select('*');
+		$query->from('#__bootcamp_leaderboards_teams');
+		$query->where('team_id='.$db->Quote($data['team_id']));
+		$db->setQuery($query);
+		$teamInfo = $db->loadObject();
+                
+                // try to find the team member
+                $query->select('*');
+		$query->from('#__comprofiler');
+		$query->where('user_id='.$db->Quote($data['user_id']));
+		$db->setQuery($query);
+		$userInfo = $db->loadObject();
+		
+                //create and fill a new stdclass with the 
+                //form info ready to be sent to the DB
+		$results =new stdClass();
+		$results->result_id = NULL;
+                $results->result_name = $matchInfo['match_name'];
+                $results->result_leaderboard = $leaderboardInfo['leaderboard_id'];
+		$results->result_date = date("j-n-Y");
+		$results->result_team = $teamInfo['team_id'];
+		$results->result_team_member = $userInfo['user_id'];
+		$results->result_match = $matchInfo['match_id'];
+                $results->result_damage = $data['damage'];
+                $results->result_healing = $data['healing'];
+                $results->result_kills = $data['kills'];
+                $results->result_deaths = $data['deaths'];
+                $results->result_points = $data['points'];
+		$results->result_createdby = $userid;
+
+                //get DB Object to initialize DB Connection
+		$db = JFactory::getDBO();
+                //insert the new record into the table
+		$db->insertObject('#__bootcamp_leaderboards_results', $results);
+		
+		if ($db->getErrorMsg()) 
+		{
+			JError::raiseError(500, $db->getErrorMsg());
+			return false;
+		}
+                //assign the user id to a variable
+                $resultuser  = $userInfo['user_id'];
+                
+                //get current points earned this month from the Community Builder Table
+                $query = "SELECT cb_tm_points FROM #__comprofiler WHERE user_id=".$resultuser;
+                
+                //assigns those points to a new variable
+                $currenttmpts = $db->setQuery($query);
+                //adds current value to the existing value as a new variable
+                $totaltmpts = $currenttmpts + $data['points'];
+                
+                //get current points earned over the user's lifetime from the Community Builder Table
+                $query = "SELECT cb_lt_points FROM #__comprofiler WHERE user_id=".$resultuser;
+                
+                //assigns these points to a new variable
+                $currentltpts = $db->setQuery($query);
+                //adds current value to the existing value as a new variable
+                $totalltpts = $currentltpts + $data['points'];
+                
+                //update the Community Builder Table with the new values
+                $query = "  UPDATE #_comprofiler 
+                            SET cb_tm_points ='.$totaltmpts.'
+                            SET cb_lt_points ='.$totalltpts.'
+                            WHERE user_id='.$resultuser.'";
+                
+                
+                if ($db->getErrorMsg())
+                {
+                    JError::raiseError(500, $db->getErrorMsg());
+                    return false;
+                }
+                
+		$params = JComponentHelper::getParams( 'com_bootcampleaderboards' ); 
+		
+		return true;
+	}
+}
