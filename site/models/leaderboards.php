1fee2a9e86762955ecc4b1229c0b687b64da00d7
Zensoji <zensoji@gmail.com>
2013-08-19 11:15:50 +0100

    2 Model Changes left to do in /models/: 	- /fields/  	- /forms/ 	- teammembers.php - removed  - covered in other models

--- site/models/leaderboards.php
+++ site/models/leaderboards.php
@@ -1,7 +1,72 @@
 <?php
+/***************************************************************************
+ * 
+ * PvP Boot Camp Leaderboards Component
+ * @comname         com_bootcampleaderboards
+ * @package         Bootcampleaderboards.Site
+ * @subpackage      Views
+ *
+ * @copyright       (C) Copyright 2013 PvP Boot Camp. All rights reserved.
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            http://www.pvp-boot-camp.com
+ * @author          Alex Armstrong
+ *                  Lynton Steyn
+ * @author-email    zensoji@pvp-boot-camp.com
+ *                  drkfrontiers@pvp-boot-camp.com
+ * 
+ ***************************************************************************
+ * 
+ * Site's Leaderboard List Model.
+ * leaderboards.php 
+ * 
+ **************************************************************************/
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.model');
+
+class BootcampleaderboardModelLeaderboards extends JModel
+{
+	function _getLeaderboardsQuery( &$options ){
+		$db = JFactory::getDBO();
+		$leaderboard_id =   @$options['leaderboard_id'];
+		$select = '*';
+		$from = '#__bootcamp_leaderboards';
+		$wheres[] = 'leaderboard_published = 1';
+		$query = "SELECT ".$select.
+			 "\n FROM ".$from .
+			 "\n WHERE ".implode("\n AND ",$wheres).
+			 "\n ORDER BY leaderboard_name";
+		return $query;
+	}
+
+	function getLeaderboardList( $options=array() ){
+		$query = $this->_getLeaderboardsQuery($options);	
+		$result = $this->_getList($query);
+		return @$result;
+	}
+	
+	public function getMatches($leaderboard_id)
+	{	
+		$query  = 'SELECT ma.match_name, ma.match_date, ma.match_state, ma.match_logo';	
+		$query .= ' FROM #__bootcamp_leaderboards_matches ma';
+		$query .= ' WHERE ma.match_leaderboard_id ='.$leaderboard_id;
+		$query .= ' ORDER BY ordering';
+		
+		$db = $this->getDbo();
+		
+		$db->setQuery($query); 
+		return $db->loadObjectList();
+	}
+	
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$app = JFactory::getApplication();
+		
+		$params = $app->getParams();
+		$this->setState('params', $params);
+	}
+}
+
 ?>
--- site/models/teams.php
+++ site/models/teams.php
@@ -1,7 +1,72 @@
 <?php
+/***************************************************************************
+ * 
+ * PvP Boot Camp Leaderboards Component
+ * @comname         com_bootcampleaderboards
+ * @package         Bootcampleaderboards.Site
+ * @subpackage      Views
+ *
+ * @copyright       (C) Copyright 2013 PvP Boot Camp. All rights reserved.
+ * @license         http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link            http://www.pvp-boot-camp.com
+ * @author          Alex Armstrong
+ *                  Lynton Steyn
+ * @author-email    zensoji@pvp-boot-camp.com
+ *                  drkfrontiers@pvp-boot-camp.com
+ * 
+ ***************************************************************************
+ * 
+ * Site's Team List Model.
+ * teams.php 
+ * 
+ **************************************************************************/
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.model');
+
+class BootcampleaderboardModelTeams extends JModel
+{
+	function _getTeamsQuery( &$options ){
+		$db = JFactory::getDBO();
+		$team_id =   @$options['team_id'];
+		$select = '*';
+		$from = '#__bootcamp_leaderboards_teams ';
+		$wheres[] = 'team_published = 1';
+		$query = "SELECT ".$select.
+			 "\n FROM ".$from .
+			 "\n WHERE ".implode("\n AND ",$wheres).
+			 "\n ORDER BY team_name";
+		return $query;
+	}
+
+	function getTeamList( $options=array() ){
+		$query = $this->_getTeamsQuery( $options );	
+		$result = $this->_getList( $query );
+		return @$result;
+	}
+	
+	public function getMembers($team_id)
+	{	
+		$query  = 'SELECT m.user_id, m.cb_handle, m.username, m.avatar, m.cb_team_tm_points, m.cb_team_lt_points';	
+		$query .= ' FROM #__comprofiler m';
+		$query .= ' WHERE m.cb_team_memberstate in (1,2) AND m.cb_team_id ='.$team_id;
+		$query .= ' ORDER BY ordering';
+		
+		$db = $this->getDbo();
+		
+		$db->setQuery($query); 
+		return $db->loadObjectList();
+	}
+	
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$app = JFactory::getApplication();
+		
+		$params = $app->getParams();
+		$this->setState('params', $params);
+	}
+}
+
 ?>
